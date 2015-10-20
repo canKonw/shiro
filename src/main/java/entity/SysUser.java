f431e67f7e02d3bdd806c48aa8bedcd189789bbb
huhao <616991449@qq.com>
2015-10-20 19:14:45 +0800

    系统用户对象

---
+++ src/main/java/entity/SysUser.java
@@ -0,0 +1,58 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * Created by huhao on 15-10-20.
+ */
+public class SysUser {
+    private Integer id;
+
+    private String userName;
+
+    private Date birthday;
+
+    private String sex;
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
