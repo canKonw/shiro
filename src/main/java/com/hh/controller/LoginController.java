e11837f82d6fecf7e45520e3ea572609a20373ec
616991449@qq.com <aa3421540>
2015-10-23 00:14:52 +0800

    登录

--- src/main/java/com/hh/controller/LoginController.java
+++ src/main/java/com/hh/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.hh.controller;
 
+import com.hh.Exception.CustomException;
+import com.hh.entity.ActiveUser;
+import com.hh.service.SysService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,8 +18,23 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("loginController")
 public class LoginController {
+    @Autowired
+    private SysService sysService;
     @RequestMapping("/login")
-    public ModelAndView login(HttpServletRequest request,HttpServletResponse response){
+    public String login(Model model,HttpServletRequest request,HttpServletResponse response,String  username,String password,String randomcode) throws Exception{
+        String validateCode = (String)request.getAttribute("validateCode");
+        if(!validateCode.equals(randomcode)){
+           throw  new CustomException("验证码错误！");
+        }
+        ActiveUser activeUser = sysService.authenticat(username,password);
+        request.setAttribute("activeUser", activeUser);
+        model.addAttribute("activeUser",activeUser);
+        //return "first";
+        return "redirect:/first";
+    }
+
+    @RequestMapping("/getUser")
+    public ModelAndView getUser(HttpServletRequest request,HttpServletResponse response){
         return new ModelAndView("login");
     }
 }
