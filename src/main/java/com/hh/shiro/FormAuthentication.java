099961756ecee4ffe8df21cdece322003b231497
huhao <616991449@qq.com>
2015-10-27 19:07:37 +0800

    登录时不使用表单验证

--- src/main/java/com/hh/shiro/FormAuthentication.java
+++ src/main/java/com/hh/shiro/FormAuthentication.java
@@ -1,5 +1,7 @@
 package com.hh.shiro;
 
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
 import javax.servlet.ServletRequest;
@@ -9,12 +11,12 @@ import javax.servlet.http.HttpSession;
 
 /**
  * Created by Administrator on 15-10-26.
- * 进行变淡验证   需要在配置文件中进行这个类的配置
+ * 进行表单验证   需要在配置文件中进行这个类的配置
  */
 public class FormAuthentication extends FormAuthenticationFilter {
     @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
-        System.out.println("----------- i am FormAuthentication");
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        /* System.out.println("----------- i am FormAuthentication");
         // 校验验证码
         // 从session获取正确的验证码
         HttpSession session = ((HttpServletRequest)request).getSession();
@@ -30,12 +32,27 @@ public class FormAuthentication extends FormAuthenticationFilter {
             request.setAttribute("shiroLoginFailure", "randomCodeError");
             //拒绝访问，不再校验账号和密码
             return true;
-        }*/
-        if(true){
-            return true;
         }
-
-
         return super.onAccessDenied(request, response, mappedValue);
+        */
+        return true;
+    }
+
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
+        System.out.println("------ i am FormAuthentication");
+        String username = getUsername(request);
+        String password = getPassword(request);
+        String rememberMeParam = getRememberMeParam();
+        System.out.println("----------:"+rememberMeParam);
+        if (password == null) {
+            password = "";
+        }
+        boolean rememberMe = true;
+        if(rememberMeParam==null || rememberMeParam.equals("0")){
+            rememberMe = false;
+        }
+        String host = getHost(request);
+        return new UsernamePasswordToken(username, password, rememberMe, host);
     }
 }
