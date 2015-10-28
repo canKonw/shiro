16d7e4fdb62f3a2aec174a995c37812abc66c98f
huhao <616991449@qq.com>
2015-10-28 20:21:16 +0800

    获得验证码

---
+++ src/main/java/com/hh/controller/VerifyCodeController.java
@@ -0,0 +1,40 @@
+package com.hh.controller;
+
+import com.hh.util.VerifyCodeUtil;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by hh on 15-10-28.
+ * 验证码
+ */
+@Component
+@RequestMapping("/")
+public class VerifyCodeController {
+    private Logger logger =Logger.getLogger(VerifyCodeController.class);
+
+    @RequestMapping("/getValidateCode")
+    public void getValidateCode(HttpServletRequest request,HttpServletResponse response) throws IOException{
+        //设置页面不缓存
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        String verifyCode = VerifyCodeUtil.generateTextCode(VerifyCodeUtil.TYPE_NUM_ONLY, 4, null);
+        //将验证码放到HttpSession里面
+        request.getSession().setAttribute("verifyCode", verifyCode);
+        logger.error("本次生成的验证码为[" + verifyCode + "],已存放到HttpSession中");
+        //设置输出的内容的类型为JPEG图像
+        response.setContentType("image/jpeg");
+        BufferedImage bufferedImage = VerifyCodeUtil.generateImageCode(verifyCode, 90, 30, 3, true, Color.WHITE, Color.BLACK, null);
+        //写给浏览器
+        ImageIO.write(bufferedImage, "JPEG", response.getOutputStream());
+    }
+}
